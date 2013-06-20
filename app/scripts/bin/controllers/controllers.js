c9c52bab9b116ba76070838e0571ff6c30954d85
johanrox <johanrox@ef392f78-ee14-0410-84e5-9db46baae5bb>
2013-06-20 10:06:23 +0000

    fixed indent issue for so
    git-svn-id: https://svn.spraakdata.gu.se/repos/littb/trunk/frontend_v3@74463 ef392f78-ee14-0410-84e5-9db46baae5bb

--- app/scripts/bin/controllers/controllers.js
+++ app/scripts/bin/controllers/controllers.js
@@ -1022,7 +1022,7 @@
             for (_i = 0, _len = _ref.length; _i < _len; _i++) {
               article = _ref[_i];
               _results.push({
-                baseform: $("grundform-clean", article).text(),
+                baseform: $("grundform-clean:first", article).text(),
                 lexemes: util.getInnerXML(article)
               });
             }
