80ad5e681e43e184652d795f03ad3e6b16655af5
Johan Roxendal <johan.roxendal@litteraturbanken.se>
2026-07-26 05:38:13 +0200

    fix(about): disable managed page prefetch

--- nuxt/app/components/about/AboutPageShell.vue
+++ nuxt/app/components/about/AboutPageShell.vue
@@ -13,13 +13,13 @@ defineProps<{ activePage: AboutPage | null }>()
 <template>
   <h1>Om Litteraturbanken</h1>
   <ul class="links">
-    <li><NuxtLink :class="{ active: activePage === 'ide' }" to="/om/ide">Intro</NuxtLink></li>{{ " " }}
-    <li><NuxtLink to="/om/organisation">Organisation</NuxtLink></li>{{ " " }}
-    <li><NuxtLink :class="{ active: activePage === 'hjalp' }" to="/om/hjalp">Hjälp</NuxtLink></li>{{ " " }}
-    <li><NuxtLink :class="{ active: activePage === 'rattigheter' }" to="/om/rattigheter">Rättigheter</NuxtLink></li>{{ " " }}
-    <li><NuxtLink :class="{ active: activePage === 'tack' }" to="/om/tack">Tack</NuxtLink></li>{{ " " }}
-    <li><NuxtLink :class="{ active: activePage === 'statistik' }" to="/om/statistik">Statistik</NuxtLink></li>{{ " " }}
-    <li><NuxtLink :class="{ active: activePage === 'kontakt' }" to="/om/kontakt">Kontakt</NuxtLink></li>
+    <li><NuxtLink :class="{ active: activePage === 'ide' }" :prefetch="false" to="/om/ide">Intro</NuxtLink></li>{{ " " }}
+    <li><NuxtLink :prefetch="false" to="/om/organisation">Organisation</NuxtLink></li>{{ " " }}
+    <li><NuxtLink :class="{ active: activePage === 'hjalp' }" :prefetch="false" to="/om/hjalp">Hjälp</NuxtLink></li>{{ " " }}
+    <li><NuxtLink :class="{ active: activePage === 'rattigheter' }" :prefetch="false" to="/om/rattigheter">Rättigheter</NuxtLink></li>{{ " " }}
+    <li><NuxtLink :class="{ active: activePage === 'tack' }" :prefetch="false" to="/om/tack">Tack</NuxtLink></li>{{ " " }}
+    <li><NuxtLink :class="{ active: activePage === 'statistik' }" :prefetch="false" to="/om/statistik">Statistik</NuxtLink></li>{{ " " }}
+    <li><NuxtLink :class="{ active: activePage === 'kontakt' }" :prefetch="false" to="/om/kontakt">Kontakt</NuxtLink></li>
   </ul>
   <slot />
 </template>
